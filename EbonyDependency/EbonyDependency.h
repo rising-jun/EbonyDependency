d19bb53a2d8eda56e72b1394a76b2b4da8adf983
kimdongjune <jun36817@naver.com>
2022-04-24 16:33:15 +0900

    Initial Commit

---
+++ EbonyDependency/EbonyDependency.h
@@ -0,0 +1,18 @@
+//
+//  EbonyDependency.h
+//  EbonyDependency
+//
+//  Created by 김동준 on 2022/04/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EbonyDependency.
+FOUNDATION_EXPORT double EbonyDependencyVersionNumber;
+
+//! Project version string for EbonyDependency.
+FOUNDATION_EXPORT const unsigned char EbonyDependencyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EbonyDependency/PublicHeader.h>
+
+
